fe02a328d92343ec01bccc1d3a2e273bfb9c0457
Vlad <vladoff800@gmail.com>
2026-02-13 21:25:18 +0300

    fix masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(card_number: str) -> str:
     """ Маскирует номер карты в формате XXXX XX** **** XXXX."""
-    mask_card_number = card_number[:4] + " " + card_number[4:6] + "** ****" + card_number[-4:]
+    mask_card_number = card_number[:-12] + " " + card_number[-12:-10] + "** **** " + card_number[-4:]
     return mask_card_number
 
 
