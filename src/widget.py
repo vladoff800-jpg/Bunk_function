08e82a250b2916c115f877c1e4e19cfd8586259d
Vlad <vladoff800@gmail.com>
2026-02-13 21:26:20 +0300

    add widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,16 @@
+from src import masks
+
+
 def mask_account_card(number: str) -> str:
     """Маскирует информацию карты или счета"""
     if number[:4] == "Счет":
-        musk_number = "Счет " + "**" + number[-4:]
+        musk_number = "Счет " + masks.get_mask_account(number)
     else:
-        musk_number = number[:-12] + " " + number[-12:-10] + "** **** " + number[-4:]
+        musk_number = masks.get_mask_card_number(number)
     return musk_number
+
+
+def get_date(date: str) -> str:
+    """Форматирует время ДД.ММ.ГГГГ"""
+    form_date = date[8:10] + "." + date[5:7] + "." + date[:4]
+    return form_date
